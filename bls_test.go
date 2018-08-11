d72d6b1a597898c011b4450c16a0515e2342409b
Terence Tsao <terence@prysmaticlabs.com>
2018-08-11 16:45:18 -0700

    Trying Nick-U lib. Setting up new pairing

--- bls_test.go
+++ bls_test.go
@@ -2,30 +2,14 @@ package bls
 
 import (
 	"testing"
-	"golang.org/x/crypto/blake2s"
+	"github.com/Nik-U/pbc"
+
 )
 
 func TestOneTimeSignVerify(t *testing.T) {
 
-	// Assume we are using type A param
-	// See: https://crypto.stanford.edu/pbc/manual/ch08s03.html
-	params := GenParamsTypeA(360, 1024)
-	paring := GenPairing(params)
-	system , _ := GenSystem(paring)
-	pubKey, privKey, err := GenKeys(system)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Hash the message, sign the hash to get signature
-	hash, err := blake2s.New256([]byte("I love bls!"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	var hashByte [32]byte
-	copy(hashByte[:], hash.Sum(nil))
-	sig := Sign(hashByte, privKey)
-
-	// Verify the output of signature and the hashed message matches public key
-	t.Log(Verify(sig, hashByte, pubKey))
+	// Setting up new paring
+	params := pbc.GenerateA(160, 512)
+	paring := params.NewPairing()
+	g1 := paring.NewG1().Rand()
 }
