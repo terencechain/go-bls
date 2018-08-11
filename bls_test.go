93c7df239013ab083b6a1fa204f67d6336c4da88
Terence Tsao <terence@prysmaticlabs.com>
2018-08-11 16:51:38 -0700

    participants should share params and generator

--- bls_test.go
+++ bls_test.go
@@ -6,10 +6,23 @@ import (
 
 )
 
+type data struct {
+	msg string
+	sig []byte
+}
+
 func TestOneTimeSignVerify(t *testing.T) {
 
 	// Setting up new paring
 	params := pbc.GenerateA(160, 512)
 	paring := params.NewPairing()
-	g1 := paring.NewG1().Rand()
+	generator := paring.NewG1().Rand()
+
+	// We should send the generator to the participants
+	sharedParams := params.String()
+	sharedGenerator := generator.Bytes()
+
+	// Establish a channel to share data across
+	messageChannel := make(chan *data)
 }
+
